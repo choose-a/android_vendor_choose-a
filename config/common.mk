026f007ff255bccfcf276a99434fd33d07318c95
Humberto Borba <humberos@gmail.com>
2017-10-08 12:34:44 -0300

    vendor: choose-a: Add backuptool from Omni
    Copy/paste all files from OmniROM project.
    
    Signed-off-by: Humberto Borba <humberos@gmail.com>
    Change-Id: Ica1689b4ced12fb339059a434a712f5b6c76190b

--- config/common.mk
+++ config/common.mk
@@ -8,6 +8,17 @@ PRODUCT_COPY_FILES += \
 PRODUCT_PROPERTY_OVERRIDES += \
     persist.sys.root_access=1
 
+# Backup Tool
+PRODUCT_COPY_FILES += \
+    vendor/choose-a/prebuilt/bin/backuptool.sh:system/bin/backuptool.sh \
+    vendor/choose-a/prebuilt/bin/backuptool.functions:system/bin/backuptool.functions \
+    vendor/choose-a/prebuilt/bin/50-hosts.sh:system/addon.d/50-hosts.sh \
+    vendor/choose-a/prebuilt/bin/blacklist:system/addon.d/blacklist
+
+# Backup Services whitelist
+PRODUCT_COPY_FILES += \
+    vendor/choose-a/prebuilt/etc/sysconfig/backup.xml:system/etc/sysconfig/backup.xml
+
 # Additional packages
 -include vendor/choose-a/config/packages.mk
 
