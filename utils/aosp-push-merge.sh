1ab05b16850ed615b8c6a357df84a4ae33142951
Humberto Borba <humberos@gmail.com>
2017-10-08 12:28:56 -0300

    vendor: choose-a: Add choose-a utils
    Change-Id: Ife3c899b1ea867eb93f3d846e6646179bd5690a5
    Signed-off-by: Humberto Borba <humberos@gmail.com>

---
+++ utils/aosp-push-merge.sh
@@ -0,0 +1,48 @@
+#!/bin/bash
+#
+# Copyright (C) 2016 OmniROM Project
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#      http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+#
+echo -e "Enter the username"
+read username
+
+url="review.choose-a.name"
+port="29418"
+branch="choose-8.0"
+
+cd ../../../
+
+while read path;
+    do
+
+    project=`echo android_${path} | sed -e 's/\//\_/g'`
+    if [ "${project}" == "android_build_make" ] ; then
+        project="android_build"
+    fi
+
+    echo ""
+    echo "====================================================================="
+    echo " PROJECT: ${project} -> [ ${path}/ ]"
+    echo ""
+
+    cd $path;
+
+    echo " Pushing..."
+    echo " git push --no-thin ssh://${username}@${url}:${port}/choose-a/${project} HEAD:refs/heads/${branch}"
+    git push --no-thin ssh://${username}@${url}:${port}/choose-a/${project} HEAD:refs/heads/${branch}
+    echo ""
+
+    cd - > /dev/null
+
+done < vendor/choose-a/utils/aosp-forked-list
