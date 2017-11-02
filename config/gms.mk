b796997d8c5cb7289ca4265758ee89b4fbe01fe8
David <Davidteri91@gmail.com>
2017-11-02 10:05:19 +0100

    vendor: add DRM widevine
    Google Play Videos (Downloads tested and working)
    Netflix (Downloads tested and working)
    
    Change-Id: Ia1ec04036cc0a79a19c71de431170b9ee4643338
    Signed-off-by: David Viteri <davidteri91@gmail.com>

---
+++ config/gms.mk
@@ -0,0 +1,6 @@
+VENDOR_CHOOSE_PATH := vendor/choose-a
+
+
+# DRM
+PRODUCT_COPY_FILES +=  \
+    $(VENDOR_CHOOSE_PATH)/prebuilt/common/vendor/lib/mediadrm/libwvdrmengine.so:system/vendor/lib/mediadrm/libwvdrmengine.so
