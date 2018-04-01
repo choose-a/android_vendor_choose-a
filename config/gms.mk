e462c4b89a9d42f0abfdd6d86ad71e6d6ac04e85
David <Davidteri91@gmail.com>
2018-04-01 11:35:02 +0200

    vendor: update widevine
    taken from taimen march release
    
    Change-Id: Ic4a6a6ad2a9ef1d7ece53325730137e16c42e742
    Signed-off-by: David Viteri <davidteri91@gmail.com>

--- config/gms.mk
+++ config/gms.mk
@@ -45,6 +45,8 @@ PRODUCT_PACKAGES += \
 
 # DRM
 PRODUCT_COPY_FILES +=  \
-    $(VENDOR_CHOOSE_PATH)/prebuilt/common/vendor/lib/mediadrm/libwvdrmengine.so:system/vendor/lib/mediadrm/libwvdrmengine.so
+    $(VENDOR_CHOOSE_PATH)/prebuilt/common/vendor/bin/hw/android.hardware.drm@1.0-service.widevine:$(TARGET_COPY_OUT_VENDOR)/bin/hw/android.hardware.drm@1.0-service.widevine \
+    $(VENDOR_CHOOSE_PATH)/prebuilt/common/vendor/etc/init/android.hardware.drm@1.0-service.widevine.rc:$(TARGET_COPY_OUT_VENDOR)/etc/init/android.hardware.drm@1.0-service.widevine.rc \
+    $(VENDOR_CHOOSE_PATH)/prebuilt/common/vendor/lib/libwvhidl.so:$(TARGET_COPY_OUT_VENDOR)/lib/libwvhidl.so
 
 $(call inherit-product-if-exists, vendor/opengapps/build/opengapps-packages.mk)
