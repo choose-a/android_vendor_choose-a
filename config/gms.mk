e39d4aece4d8df659f3149f61b429098b1572e80
Erik Castricum <git@cas-online.nl>
2018-05-24 22:56:12 +0200

    add exfat and libfuse
    thanks @alviteri
    
    Change-Id: Iadf3e2b118019d16b65c1c71193cee668a861c57
    Signed-off-by: Erik Castricum <git@cas-online.nl>

--- config/gms.mk
+++ config/gms.mk
@@ -6,3 +6,14 @@ PRODUCT_COPY_FILES +=  \
     $(VENDOR_CHOOSE_PATH)/prebuilt/common/vendor/bin/hw/android.hardware.drm@1.0-service.widevine:$(TARGET_COPY_OUT_VENDOR)/bin/hw/android.hardware.drm@1.0-service.widevine \
     $(VENDOR_CHOOSE_PATH)/prebuilt/common/vendor/etc/init/android.hardware.drm@1.0-service.widevine.rc:$(TARGET_COPY_OUT_VENDOR)/etc/init/android.hardware.drm@1.0-service.widevine.rc \
     $(VENDOR_CHOOSE_PATH)/prebuilt/common/vendor/lib/libwvhidl.so:$(TARGET_COPY_OUT_VENDOR)/lib/libwvhidl.so
+
+# libfuse
+PRODUCT_PACKAGES += \
+    libfuse
+
+# exfat
+PRODUCT_PACKAGES += \
+    fsck.exfat \
+    libexfat \
+    mkfs.exfat \
+    mount.exfat
