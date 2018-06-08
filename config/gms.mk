droncheg/vendor_google

vendor: add DRM widevine
Google Play Videos (Downloads tested and working)
Netflix (Downloads tested and working)

Signed-off-by: David Viteri <davidteri91@gmail.com>

products/gms.mk
@@ -59,6 +59,10 @@ PRODUCT_PACKAGES += \
     mkfs.exfat \
     mount.exfat
 
+# DRM
+PRODUCT_COPY_FILES +=  \
+    $(VENDOR_SONYAOSP_PATH)/prebuilt/common/vendor/lib/mediadrm/libwvdrmengine.so:system/vendor/lib/mediadrm/libwvdrmengine.so
+
 # Audio (Ringtones - Not windy devices allowed)
 ifneq ($(filter-out aosp_sgp511 aosp_sgp611 aosp_sgp712, $(TARGET_PRODUCT)),)
 PRODUCT_PROPERTY_OVERRIDES += \

choose-a/android_vendor_choose-a

Change-Id: Ia1ec04036cc0a79a19c71de431170b9ee4643338

config/gms.mk
@@ -1,5 +1,9 @@
 VENDOR_CHOOSE_PATH := vendor/choose-a
 
+# DRM
+PRODUCT_COPY_FILES +=  \
+    $(VENDOR_CHOOSE_PATH)/prebuilt/common/vendor/lib/mediadrm/libwvdrmengine.so:system/vendor/lib/mediadrm/libwvdrmengine.so
+
 # Google property overides
 PRODUCT_PROPERTY_OVERRIDES += \
     ro.url.legal=http://www.google.com/intl/%s/mobile/android/basic/phone-legal.html \