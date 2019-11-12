dcb4a4fceebe128e648d8d150e7fb59399429c9c
Erik Castricum <git@cas-online.nl>
2019-11-12 23:58:18 +0100

    include soong config
    Signed-off-by: Erik Castricum <git@cas-online.nl>
    Change-Id: I73eefcd339e981f56b31b3ebc2e3cd7ec3bbba81

--- config/BoardConfigChoose.mk
+++ config/BoardConfigChoose.mk
@@ -1,6 +1,7 @@
 # Include some configs
 
 include vendor/choose-a/config/BoardConfigKernel.mk
+include vendor/choose-a/config/BoardConfigSoong.mk
 
 ifeq ($(BOARD_USES_QCOM_HARDWARE),true)
 include vendor/choose-a/config/BoardConfigQcom.mk
