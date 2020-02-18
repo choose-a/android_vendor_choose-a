15e1463a50f3af7b188d92cde9f96855116bb695
Erik Castricum <git@cas-online.nl>
2020-02-18 11:40:58 +0100

    soong: A very dirty kang from Carbon ;)
    TODO: split into the proper commits
    
    Signed-off-by: Erik Castricum <git@cas-online.nl>
    Change-Id: Ia55871ae81e228f73fa79b616c6a926c7d712dc8

--- build/soong/soong_config.mk
+++ build/soong/soong_config.mk
@@ -1,13 +1,37 @@
 add_json_str_omitempty = $(if $(strip $(2)),$(call add_json_str, $(1), $(2)))
+add_json_val_default = $(call add_json_val, $(1), $(if $(strip $(2)), $(2), $(3)))
 
-_json_contents := $(_json_contents)    "Choose":{$(newline)
+_json_contents := $(_json_contents)    "Carbon":{$(newline)
 
-# See build/core/soong_config.mk for the add_json_* functions you can use here.
-$(call add_json_bool, Healthd_use_battery_info, $(filter true,$(HEALTHD_USE_BATTERY_INFO)))
-$(call add_json_bool, TargetNeedsHWCOnFirstRef, $(filter true,$(TARGET_NEEDS_HWC_ONFIRSTREF)))
-$(call add_json_bool, Uses_metadata_as_fde_key, $(filter true,$(TARGET_USES_METADATA_AS_FDE_KEY)))
-$(call add_json_bool, Target_use_sdclang,       $(filter true,$(TARGET_USE_SDCLANG)))
+$(call add_json_bool,	Needs_text_relocations,						$(filter true,$(TARGET_NEEDS_PLATFORM_TEXT_RELOCATIONS)))
+$(call add_json_bool,	Should_wait_for_qsee,						$(filter true,$(TARGET_KEYMASTER_WAIT_FOR_QSEE)))
+$(call add_json_bool,	Supports_hw_fde,							$(filter true,$(TARGET_HW_DISK_ENCRYPTION)))
+$(call add_json_bool,	Supports_hw_fde_perf,						$(filter true,$(TARGET_HW_DISK_ENCRYPTION_PERF)))
+$(call add_json_str_omitempty, 	Target_init_vendor_lib,			 $(TARGET_INIT_VENDOR_LIB))
+$(call add_json_bool,	Target_uses_eigen,							$(filter true,$(if $(strip $(TARGET_USES_QML)),false,true)))
+$(call add_json_bool,	Target_uses_qml,							$(filter true,$(TARGET_USES_QML)))
+$(call add_json_bool,	Uses_generic_camera_parameter_library,		$(if $(TARGET_SPECIFIC_CAMERA_PARAMETER_LIBRARY),,true))
+$(call add_json_bool,	Uses_qti_camera_device,						$(filter true,$(TARGET_USES_QTI_CAMERA_DEVICE)))
+$(call add_json_bool,	Uses_qcom_bsp_legacy,						$(filter true,$(TARGET_USES_QCOM_BSP_LEGACY)))
+$(call add_json_bool,	Uses_qcom_um_family, 						$(filter true,$(TARGET_USES_QCOM_UM_FAMILY)))
+$(call add_json_bool,	Uses_qcom_um_3_18_family, 					$(filter true,$(TARGET_USES_QCOM_UM_3_18_FAMILY)))
+$(call add_json_bool,	Uses_qcom_um_4_4_family, 					$(filter true,$(TARGET_USES_QCOM_UM_4_4_FAMILY)))
+$(call add_json_bool,	Uses_qcom_um_4_9_family, 					$(filter true,$(TARGET_USES_QCOM_UM_4_9_FAMILY)))
+$(call add_json_bool,	Uses_qcom_um_4_14_family, 					$(filter true,$(TARGET_USES_QCOM_UM_4_14_FAMILY)))
+$(call add_json_bool,	Has_legacy_camera_hal1,						$(filter true,$(TARGET_HAS_LEGACY_CAMERA_HAL1)))
+$(call add_json_bool,	Supports_extended_compress_format,			$(filter true,$(AUDIO_FEATURE_ENABLED_EXTENDED_COMPRESS_FORMAT)))
 
+$(call add_json_str_omitempty,	Additional_gralloc_10_usage_bits,	$(TARGET_ADDITIONAL_GRALLOC_10_USAGE_BITS))
+$(call add_json_str_omitempty,	Target_process_sdk_version_override,$(TARGET_PROCESS_SDK_VERSION_OVERRIDE))
+
+$(call add_json_val_default,	Bootloader_message_offset,			$(BOOTLOADER_MESSAGE_OFFSET), 0)
+
+$(call add_json_str,	Java_Source_Overlays,						$(JAVA_SOURCE_OVERLAYS))
+$(call add_json_str,	QTIAudioPath,								$(call project-path-for,qcom-audio))
+$(call add_json_str,	QTIDisplayPath,								$(call project-path-for,qcom-display))
+$(call add_json_str,	QTIMediaPath,								$(call project-path-for,qcom-media))
+$(call add_json_str,	Specific_camera_parameter_library,			$(TARGET_SPECIFIC_CAMERA_PARAMETER_LIBRARY))
+$(call add_json_str,	Target_shim_libs,							$(subst $(space),:,$(TARGET_LD_SHIM_LIBS)))
 
 # This causes the build system to strip out the last comma in our nested struct, to keep the JSON valid.
 _json_contents := $(_json_contents)__SV_END
