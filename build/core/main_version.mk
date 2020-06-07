081f0929754ebd47472297e713909db6931731e1
Erik Castricum <git@cas-online.nl>
2020-06-07 16:12:49 +0200

    properties: be consistent
    erikcas@luuktop:~$ adb shell getprop | grep ro.choose
    [ro.choose-a.version]: [choose-10.0-20200607-lavender-WEEKLY]
    [ro.choose.device]: [lavender]
    
    Signed-off-by: Erik Castricum <git@cas-online.nl>
    Change-Id: I312a4be649c09fce9769eaac0519de887b412ddf

--- build/core/main_version.mk
+++ build/core/main_version.mk
@@ -1,10 +1,7 @@
 # Apply it to build.prop
 ADDITIONAL_BUILD_PROPERTIES += \
     ro.modversion=Choose-A-Rom-$(CHOOSE_VERSION) \
-    ro.choose-a.version=$(CHOOSE_VERSION) \
-    ro.romstats.url=https://stats.choose-a.name \
-    ro.romstats.name=Choose-A-Rom \
-    ro.romstats.version=$(CHOOSE_VERSION)
+    ro.choose.version=$(CHOOSE_VERSION) \
 
 # Build fingerprint
 ifneq ($(BUILD_FINGERPRINT),)
